9bff996666428062cc68354c7c5c243ecf2a0ab1
ihorekghost <158592015+ihorekghost@users.noreply.github.com>
2024-02-24 04:50:34 +0200

    Added SceneManager.cs
    Basic implementation

---
+++ SceneManager.cs
@@ -0,0 +1,46 @@
+using Godot;
+using System;
+
+
+namespace GH
+{
+    namespace SceneManager
+    {
+        [GlobalClass]
+        public partial class SceneManager : Node
+        {
+            [Export]
+            public Node CurrentScene { get; private set; } = null;
+
+            public void QueueSwitchScene(PackedScene newScene)
+            {
+                CallDeferred(MethodName.SwitchScene, newScene.Instantiate());
+            }
+
+            public void QueueSwitchScene(Node newSceneNode)
+            {
+                CallDeferred(MethodName.SwitchScene, newSceneNode);
+            }
+
+
+
+            public void SwitchScene(Node newSceneNode)
+            {
+
+                if (newSceneNode != CurrentScene)
+                {
+                    if (CurrentScene != null)
+                    {
+                        RemoveChild(CurrentScene);
+                        CurrentScene.Free();
+                    }
+
+                    CurrentScene = newSceneNode;
+
+                    AddChild(newSceneNode);
+                }
+            }
+        }
+
+    }
+}
